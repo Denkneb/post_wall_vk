51eaa57ba1841c632b448905e9ce5533038eaab5
dkislinskiy <denkneb@gmail.com>
2017-08-04 20:29:39 +0700

    Implementation app

--- vk_com/admin.py
+++ vk_com/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from vk_com.models import NewsForVk
+
+
+class NewsForVkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'news_id', 'vk_post_id', 'is_published']
+
+    class Meta:
+        model = NewsForVk
+
+admin.site.register(NewsForVk, NewsForVkAdmin)
--- vk_com/models.py
+++ vk_com/models.py
@@ -1,2 +1,20 @@
 from django.db import models
 
+
+class VkAccess(models.Model):
+    vk_group_id = models.CharField(max_length=100)
+    vk_user_id = models.CharField(max_length=100)
+    vk_client_id = models.CharField(max_length=100)
+    vk_client_secret = models.CharField(max_length=100)
+    vk_service_secret = models.CharField(max_length=100)
+    vk_access_token = models.CharField(max_length=100, null=True)
+
+
+class NewsForVk(models.Model):
+    title = models.CharField(max_length=255)
+    news_id = models.IntegerField()
+    is_published = models.BooleanField(default=False)
+    vk_post_id = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return '%s' % self.title
