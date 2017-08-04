fcbdf7a2fa0a4af913432dd469f93f2348e59314
dkislinskiy <denkneb@gmail.com>
2017-08-04 20:30:21 +0700

    Settings Celery

--- post_wall_vk/settings.py
+++ post_wall_vk/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'vk_com',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +102,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BROKER_HOST = "127.0.0.1"
+
+BROKER_PORT = 5672
+
+BROKER_USER = "broker_user"
+
+BROKER_PASSWORD = "broker_pass"
+
+BROKER_VHOST = "/"
+
+BROKER_URL = 'amqp://broker_user:broker_pass@127.0.0.1:5672/'
